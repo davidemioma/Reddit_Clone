6d239ae0f5b7ad39d0992b33bcd1a53fe109f1bc
david emioma <davidemiomauche@gmail.com>
2023-07-13 16:59:18 +0100

    Implemented voting system

--- app/api/subreddit/post/vote/route.ts
+++ app/api/subreddit/post/vote/route.ts
@@ -5,8 +5,39 @@ import { CachedPost } from "@/types/redis";
 import { NextResponse } from "next/server";
 import { getSession } from "@/actions/getSession";
 import { PostVoteValidator } from "@/libs/validator/vote";
+import { Post, User, Vote, VoteType } from "@prisma/client";
 
 const CACHE_AFTER_UPVOTE = 1;
+
+const cachePost = async (
+  postId: string,
+  post: Post & {
+    author: User;
+    votes: Vote[];
+  },
+  voteType: VoteType
+) => {
+  const voteCount = post.votes.reduce((acc, vote) => {
+    if (vote.type === "UP") return acc + 1;
+
+    if (vote.type === "DOWN") return acc - 1;
+
+    return acc;
+  }, 0);
+
+  if (voteCount >= CACHE_AFTER_UPVOTE) {
+    const cachedPost: CachedPost = {
+      id: post.id,
+      title: post.title,
+      authorUsername: post.author.username || "",
+      content: JSON.stringify(post.content),
+      currentVote: voteType,
+      createdAt: post.createdAt,
+    };
+
+    await redis.hset(`post:${postId}`, cachedPost);
+  }
+};
 
 export async function PATCH(request: Request) {
   try {
@@ -52,27 +83,7 @@ export async function PATCH(request: Request) {
           },
         });
 
-        //Get the vote count for caching.
-        const voteCount = post.votes.reduce((acc, vote) => {
-          if (vote.type === "UP") return acc + 1;
-
-          if (vote.type === "DOWN") return acc - 1;
-
-          return acc;
-        }, 0);
-
-        if (voteCount >= CACHE_AFTER_UPVOTE) {
-          const cachedPost: CachedPost = {
-            id: post.id,
-            title: post.title,
-            authorUsername: post.author.username || "",
-            content: JSON.stringify(post.content),
-            currentVote: voteType,
-            createdAt: post.createdAt,
-          };
-
-          await redis.hset(`post:${postId}`, cachedPost);
-        }
+        cachePost(postId, post, voteType);
 
         return NextResponse.json("Vote deleted");
       } else {
@@ -88,27 +99,7 @@ export async function PATCH(request: Request) {
           },
         });
 
-        //Get the vote count for caching.
-        const voteCount = post.votes.reduce((acc, vote) => {
-          if (vote.type === "UP") return acc + 1;
-
-          if (vote.type === "DOWN") return acc - 1;
-
-          return acc;
-        }, 0);
-
-        if (voteCount >= CACHE_AFTER_UPVOTE) {
-          const cachedPost: CachedPost = {
-            id: post.id,
-            title: post.title,
-            authorUsername: post.author.username || "",
-            content: JSON.stringify(post.content),
-            currentVote: voteType,
-            createdAt: post.createdAt,
-          };
-
-          await redis.hset(`post:${postId}`, cachedPost);
-        }
+        cachePost(postId, post, voteType);
 
         return NextResponse.json("Vote changed");
       }
@@ -121,27 +112,7 @@ export async function PATCH(request: Request) {
         },
       });
 
-      //Get the vote count for caching.
-      const voteCount = post.votes.reduce((acc, vote) => {
-        if (vote.type === "UP") return acc + 1;
-
-        if (vote.type === "DOWN") return acc - 1;
-
-        return acc;
-      }, 0);
-
-      if (voteCount >= CACHE_AFTER_UPVOTE) {
-        const cachedPost: CachedPost = {
-          id: post.id,
-          title: post.title,
-          authorUsername: post.author.username || "",
-          content: JSON.stringify(post.content),
-          currentVote: voteType,
-          createdAt: post.createdAt,
-        };
-
-        await redis.hset(`post:${postId}`, cachedPost);
-      }
+      cachePost(postId, post, voteType);
 
       return NextResponse.json("Vote created");
     }
